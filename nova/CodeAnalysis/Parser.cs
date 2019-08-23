affd9eab103d42165e704a5b44f0d7893a0f4017
Luqp <luceroqp@outlook.com>
2019-08-22 23:00:33 -0400

    Separate code in files

---
+++ nova/CodeAnalysis/Parser.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+
+namespace Nova.CodeAnalysis
+{
+    class Parser
+    {
+        private readonly SyntaxToken[] tokens;
+        private int position;
+        private List<string> diagnostics = new List<string>();
+        private ExpressionSyntax ParseExpression()
+        {
+            return ParseTerm();
+        }
+
+        public Parser(string text)
+        {
+            var tokens = new List<SyntaxToken>();
+
+            Lexer lexer = new Lexer(text);
+            SyntaxToken token;
+            do
+            {
+                token = lexer.NextToken();
+                if (token.Kind != SyntaxKind.WhiteSpaceToken &&
+                    token.Kind != SyntaxKind.BadToken)
+                {
+                    tokens.Add(token);
+                }
+            } while (token.Kind != SyntaxKind.EndOfFileToken);
+
+            this.tokens = tokens.ToArray();
+            this.diagnostics.AddRange(lexer.Diagnostics);
+        }
+        public IEnumerable<string> Diagnostics => diagnostics;
+        private SyntaxToken Peek(int offset)
+        {
+            int index = position + offset;
+            if (index >= tokens.Length)
+                return tokens[tokens.Length - 1];
+            
+            return tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
+
+        private SyntaxToken NextToken()
+        {
+            var current = Current;
+            position++;
+            return current;
+        }
+
+        private SyntaxToken Match(SyntaxKind kind)
+        {
+            if (Current.Kind == kind)
+                return NextToken();
+
+            diagnostics.Add($"ERROR: Unexpected token <{Current.Kind}>, expected <{kind}>");
+            return new SyntaxToken(kind, Current.Position, null, null);
+        }
+
+        public SyntaxTree Parse()
+        {
+            ExpressionSyntax expression = ParseTerm();
+            SyntaxToken endOfFileToken = Match(SyntaxKind.EndOfFileToken);
+            return new SyntaxTree(diagnostics, expression, endOfFileToken);
+        }
+
+        private ExpressionSyntax ParseTerm()
+        {
+            var left = ParseFactor();
+            while (Current.Kind == SyntaxKind.PlusToken ||
+                   Current.Kind == SyntaxKind.MinusToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParseFactor();
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
+            }
+            return left;
+        }
+
+        private ExpressionSyntax ParseFactor()
+        {
+            var left = ParsePrimaryExpression();
+            while (Current.Kind == SyntaxKind.StarToken ||
+                   Current.Kind == SyntaxKind.SlashToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
+            }
+            return left;
+        }
+
+        private ExpressionSyntax ParsePrimaryExpression()
+        {
+            if (Current.Kind == SyntaxKind.OpenParenthesisToken)
+            {
+                SyntaxToken left = NextToken();
+                ExpressionSyntax expression = ParseExpression();
+                SyntaxToken right = Match(SyntaxKind.CloseParenthesisToken);
+                return new ParenthesizedExpressionSyntax(left, expression, right);
+            }
+            var numberToken = Match(SyntaxKind.NumberToken);
+            return new NumberExpressionSyntax(numberToken);
+        }
+    }
+}
