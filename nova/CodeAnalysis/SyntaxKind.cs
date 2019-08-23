399d438c9f6ebfcc873eea0efb73ff0b536ae072
windyludev <windyludev@outlook.com>
2019-08-22 23:00:33 -0400

    Separate code in files

---
+++ nova/CodeAnalysis/SyntaxKind.cs
@@ -0,0 +1,19 @@
+namespace Nova.CodeAnalysis
+{
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression,
+        ParenthesizedExpression
+    }
+}
---
+++ nova/CodeAnalysis/SyntaxTree.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nova.CodeAnalysis
+{
+    sealed class SyntaxTree
+    {
+        public SyntaxTree(IEnumerable<string> diagnostics, ExpressionSyntax root, SyntaxToken endOfFileToken)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOfFileToken = endOfFileToken;
+        }
+
+        public IReadOnlyList<string> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOfFileToken { get; }
+
+        public static SyntaxTree Parse(string text)
+        {
+            Parser parser = new Parser(text);
+            return parser.Parse();
+        }
+    }
+}
