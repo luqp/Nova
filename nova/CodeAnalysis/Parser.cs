23c0ab578c67c8b51216e17b93bf1c0f1b3a7e84
Luqp <luceroqp@outlook.com>
2019-08-28 16:38:29 -0400

    Move precedences to SyntaxFacts

--- nova/CodeAnalysis/Parser.cs
+++ nova/CodeAnalysis/Parser.cs
@@ -67,7 +67,7 @@ namespace Nova.CodeAnalysis
             var left = ParsePrimaryExpression();
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
                 
@@ -77,23 +77,6 @@ namespace Nova.CodeAnalysis
             }
 
             return left;
-        }
-
-        private int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch(kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-
-                default:
-                    return 0;                
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
