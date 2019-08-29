2de0108ce6e3a159dec4431000f0185906e3c734
Luqp <luceroqp@outlook.com>
2019-08-29 01:01:43 -0400

    Add Binder class, logic to type-Checking

--- nova/CodeAnalysis/Binding/Binder.cs
+++ nova/CodeAnalysis/Binding/Binder.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Nova.CodeAnalysis.Syntax;
 
 namespace Nova.CodeAnalysis.Binding
 {
@@ -71,4 +73,76 @@ namespace Nova.CodeAnalysis.Binding
         public BoundExpression Right { get; }
     }
 
+    internal sealed class Binder
+    {
+        public BoundExpression BindExpression(ExpressionSyntax syntax)
+        {
+            switch (syntax.Kind)
+            {
+                case SyntaxKind.LiteralExpression:
+                    return BindLiteralExpression((LiteralExpressionSyntax)syntax);
+                case SyntaxKind.UnaryExpression:
+                    return BindUnaryExpression((UnaryExpressionSyntax)syntax);
+                case SyntaxKind.BinaryExpression:
+                    return BindBinaryExpression((BinaryExpressionSyntax)syntax);
+                default:
+                    throw new Exception($"Unexpected syntax {syntax.Kind}");
+            }
+        }
+
+        private BoundExpression BindLiteralExpression(LiteralExpressionSyntax syntax)
+        {
+            var value = syntax.LiteralToken.Value as int? ?? 0;
+            return new BoundLiteralExpression(value);
+        }
+
+        private BoundExpression BindUnaryExpression(UnaryExpressionSyntax syntax)
+        {
+            var boundOperand = BindExpression(syntax.Operand);
+            var boundOperatorKind = BindUnaryOperatorKind(syntax.OperatorToken.Kind, boundOperand.Type);
+            return new BoundUnaryExpression(boundOperatorKind.Value, boundOperand);
+        }
+
+        private BoundExpression BindBinaryExpression(BinaryExpressionSyntax syntax)
+        {
+            var boundLeft = BindExpression(syntax.Left);
+            var boundRight = BindExpression(syntax.Right);
+            var boundOperatorKind = BindBinaryOperatorKind(syntax.OperatorToken.Kind, boundLeft.Type, boundRight.Type);
+            return new BoundBinaryExpression(boundLeft, boundOperatorKind.Value, boundRight);
+        }
+
+        private BoundUnaryOperatorKind? BindUnaryOperatorKind(SyntaxKind kind, Type operandType)
+        {
+            if (operandType != typeof(int))
+                return null;
+            switch (kind)
+            {
+                case SyntaxKind.PlusToken:
+                    return BoundUnaryOperatorKind.Identity;
+                case SyntaxKind.MinusToken:
+                    return BoundUnaryOperatorKind.Negation;
+                default:
+                    throw new Exception($"Unexpected unary operator <{kind}>");
+            }
+        }
+
+        private BoundBinaryOperatorKind? BindBinaryOperatorKind(SyntaxKind kind, Type leftType, Type rightType)
+        {
+            if (leftType != typeof(int) || rightType != typeof(int))
+                return null;
+            switch (kind)
+            {
+                case SyntaxKind.PlusToken:
+                    return BoundBinaryOperatorKind.Addition;
+                case SyntaxKind.MinusToken:
+                    return BoundBinaryOperatorKind.Subtraction;
+                case SyntaxKind.StarToken:
+                    return BoundBinaryOperatorKind.Multiplication;
+                case SyntaxKind.SlashToken:
+                    return BoundBinaryOperatorKind.Division;
+                default:
+                    throw new Exception($"Unexpected binary operator <{kind}>");
+            }
+        }
+    }
 }
