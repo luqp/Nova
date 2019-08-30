ff4d49b2a56642f99c6331c6d3fc3bbace828827
Luqp <luceroqp@outlook.com>
2019-08-30 10:39:56 -0400

    Create some files to compile code on public way

---
+++ NovaLib/CodeAnalysis/Compilation.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using Nova.CodeAnalysis.Binding;
+using Nova.CodeAnalysis.Syntax;
+
+namespace Nova.CodeAnalysis
+{
+    public sealed class Compilation
+    {
+        public Compilation(SyntaxTree syntax)
+        {
+            Syntax = syntax;
+        }
+
+        public SyntaxTree Syntax { get; }
+
+        public EvaluationResult Evaluate()
+        {
+            Binder binder = new Binder();
+            BoundExpression boundExpression = binder.BindExpression(Syntax.Root);
+
+            string[] diagnostics = Syntax.Diagnostics.Concat(binder.Diagnostics).ToArray();
+            if (diagnostics.Any())
+                return new EvaluationResult(diagnostics, null);
+            
+            Evaluator evaluator = new Evaluator(boundExpression);
+            object value = evaluator.Evaluate();
+            return new EvaluationResult(Array.Empty<string>(), value);
+        }
+    }
+}
---
+++ NovaLib/CodeAnalysis/EvaluationResult.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nova.CodeAnalysis
+{
+    public sealed class EvaluationResult
+    {
+        public EvaluationResult(IEnumerable<string> diagnostics, object value)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Value = value;
+        }
+
+        public IReadOnlyList<string> Diagnostics { get; }
+        public object Value { get; }
+    }
+}
