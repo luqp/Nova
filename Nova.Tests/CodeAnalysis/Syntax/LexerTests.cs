115b90bf6bdf58d098e2259d384858aa70c3e0b2
Luqp <luceroqp@outlook.com>
2019-09-02 18:48:04 -0400

    Rename the lexer test file

--- Nova.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ Nova.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Nova.CodeAnalysis.Syntax;
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace Nova.Tests.CodeAnalysis.Syntax
 {
-    public class LexerTest
+    public class LexerTests
     {
         [Theory]
         [MemberData(nameof(GetTokensData))]
