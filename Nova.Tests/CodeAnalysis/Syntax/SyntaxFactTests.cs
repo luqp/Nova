6fc41743b575726d112badac09875ca2feafce93
Luqp <luceroqp@outlook.com>
2019-09-03 01:41:36 -0400

    Test the GetText method of each operator token

---
+++ Nova.Tests/CodeAnalysis/Syntax/SyntaxFactTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Nova.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Nova.Tests.CodeAnalysis.Syntax
+{
+    public class SyntaxFactTests
+    {
+        [Theory]
+        [MemberData(nameof(GetSyntaxKindData))]
+        public void SyntaxGetTextRoundTrips(SyntaxKind kind)
+        {
+            string text = SyntaxFacts.GetText(kind);
+
+            if (text == null)
+                return;
+            
+            IEnumerable<SyntaxToken> tokens = SyntaxTree.ParseTokens(text);
+            SyntaxToken token = Assert.Single(tokens);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+
+        public static IEnumerable<object[]> GetSyntaxKindData()
+        {
+            SyntaxKind[] kinds = (SyntaxKind[]) Enum.GetValues(typeof(SyntaxKind));
+            foreach (SyntaxKind kind in kinds)
+                yield return new object[] { kind };
+        }
+    }
+}
