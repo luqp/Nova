236d978109f86d229c25f8e0faeb6bfafce58947
Luqp <luceroqp@outlook.com>
2019-09-06 23:23:05 -0400

    Create 'BoundScope' class to declare and look for variables

---
+++ NovaLib/CodeAnalysis/Binding/BoundScope.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Nova.CodeAnalysis.Binding
+{
+    internal sealed class BoundScope
+    {
+        private Dictionary<string,VariableSymbol> variables = new Dictionary<string, VariableSymbol>();
+
+        public BoundScope(BoundScope parent)
+        {
+            Parent = parent;
+        }
+
+        public BoundScope Parent { get; }
+
+        public bool TryDeclare(VariableSymbol variable)
+        {
+            if (variables.ContainsKey(variable.Name))
+                return false;
+
+            variables.Add(variable.Name, variable);
+            return true;
+        }
+
+        public bool TryLookup(string name, out VariableSymbol variable)
+        {
+            if (variables.TryGetValue(name, out variable))
+                return true;
+            
+            if (Parent == null)
+                return false;
+            
+            return Parent.TryLookup(name, out variable);
+        }
+
+        public ImmutableArray<VariableSymbol> GetDeclaredVariables()
+        {
+            return variables.Values.ToImmutableArray();
+        }
+    }
+}
