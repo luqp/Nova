f8ef0ea18c800fb0af6914eba0c06a72783425cd
Luqp <luceroqp@outlook.com>
2019-09-18 11:01:07 -0400

    Add a command to show bound trees

--- NovaLib/CodeAnalysis/Compilation.cs
+++ NovaLib/CodeAnalysis/Compilation.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using Nova.CodeAnalysis.Binding;
@@ -53,6 +54,11 @@ namespace Nova.CodeAnalysis
             Evaluator evaluator = new Evaluator(GlobalScope.Statement, variables);
             object value = evaluator.Evaluate();
             return new EvaluationResult(ImmutableArray<Diagnostic>.Empty, value);
+        }
+
+        public void EmitTree(TextWriter writer)
+        {
+            GlobalScope.Statement.WriteTo(writer);
         }
     }
 }
