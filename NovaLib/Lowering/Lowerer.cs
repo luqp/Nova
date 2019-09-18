969380db1886bba6c466b36a55221313561d6d12
Luqp <luceroqp@outlook.com>
2019-09-18 18:31:47 -0400

    Lower for-statements into while-statements

--- NovaLib/Lowering/Lowerer.cs
+++ NovaLib/Lowering/Lowerer.cs
@@ -1,4 +1,6 @@
+using System.Collections.Immutable;
 using Nova.CodeAnalysis.Binding;
+using Nova.CodeAnalysis.Syntax;
 
 namespace Nova.CodeAnalysis.Lowering
 {
@@ -12,6 +14,35 @@ namespace Nova.CodeAnalysis.Lowering
         {
             Lowerer lowerer = new Lowerer();
             return lowerer.RewriteStatement(statement);
+        }
+
+        protected override BoundStatement RewriteForStatement(BoundForStatement node)
+        {
+            BoundVariableDeclaration variableDeclaration = new BoundVariableDeclaration(node.Variable, node.LowerBound);
+            BoundVariableExpression variableExpression = new BoundVariableExpression(node.Variable);
+
+            BoundBinaryExpression condition = new BoundBinaryExpression(
+                variableExpression,
+                BoundBinaryOperator.Bind(SyntaxKind.LessOrEqualsToken, typeof(int), typeof(int)),
+                node.UpperBound
+            );
+
+            BoundExpressionStatement increment = new BoundExpressionStatement(
+                new BoundAssignmentExpression(
+                    node.Variable,
+                    new BoundBinaryExpression(
+                        variableExpression,
+                        BoundBinaryOperator.Bind(SyntaxKind.PlusToken, typeof(int), typeof(int)),
+                        new BoundLiteralExpression(1)
+                    )
+                )
+            );
+
+            BoundBlockStatement whileBody = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(node.Body, increment));
+            BoundWhileStatement whileStatement = new BoundWhileStatement(condition, whileBody);
+
+            BoundBlockStatement result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(variableDeclaration, whileStatement));
+            return RewriteStatement(result);
         }
     }
 }
