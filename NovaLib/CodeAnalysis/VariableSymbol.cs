8def6795cf9ba46c53d6aeb2c533ff8f93dbfbde
Luqp <luceroqp@outlook.com>
2019-09-19 01:18:13 -0400

    Override 'ToString' method to return name parameter

--- NovaLib/CodeAnalysis/VariableSymbol.cs
+++ NovaLib/CodeAnalysis/VariableSymbol.cs
@@ -14,5 +14,7 @@ namespace Nova.CodeAnalysis
         public string Name { get; }
         public bool IsReadOnly { get; }
         public Type Type { get; }
+
+        public override string ToString() => Name;
     }
 }
