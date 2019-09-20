207075130d55aae7503e25eb85a7526a25e01937
Luqp <luceroqp@outlook.com>
2019-09-19 20:24:16 -0400

    Create REPL concepts and  modify 'Main' method - Create 'NovaRepl' that handles the language specific stuff

---
+++ nova/NovaRepl.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nova.CodeAnalysis;
+using Nova.CodeAnalysis.Syntax;
+using Nova.CodeAnalysis.Text;
+
+namespace Nova
+{
+    internal sealed class NovaRepl : Repl
+    {
+        private Compilation previous;
+        private bool showTree;
+        private bool showProgram;
+        private readonly Dictionary<VariableSymbol, object> variables = new Dictionary<VariableSymbol, object>();
+
+        protected override void EvaluateMetaCommand(string input)
+        {
+            switch (input)
+            {
+                case "#trees":
+                    showTree = !showTree;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(showTree ? "Enabled parse trees." : "Disabled parse trees.");
+                    break;
+                case "#showProgram":
+                    showProgram = !showProgram;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(showProgram ? "Enabled bound trees." : "Disabled bound trees.");
+                    break;
+                case "#cls":
+                    Console.Clear();
+                    break;
+                case "#reset":
+                    previous = null;
+                    variables.Clear();
+                    break;
+                default:
+                    base.EvaluateMetaCommand(input);
+                    break;
+            }
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {            
+            if (string.IsNullOrEmpty(text))
+                return false;
+            
+            SyntaxTree syntaxTree = SyntaxTree.Parse(text);
+
+            if (syntaxTree.Diagnostics.Any())
+                return false;
+            
+            return true;
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            SyntaxTree syntaxTree = SyntaxTree.Parse(text);
+
+            Compilation compilation = previous == null
+                                        ? new Compilation(syntaxTree)
+                                        : previous.ContinueWith(syntaxTree);
+
+            if (showTree)
+                syntaxTree.Root.WriteTo(Console.Out);
+
+            if (showProgram)
+                compilation.EmitTree(Console.Out);
+
+            EvaluationResult result = compilation.Evaluate(variables);
+            IReadOnlyList<Diagnostic> diagnostics = result.Diagnostics;
+
+            if (!diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+                previous = compilation;
+            }
+            else
+            {
+                foreach (Diagnostic diagnostic in diagnostics)
+                {
+                    SourceText treeText = syntaxTree.Text;
+                    int lineIndex = treeText.GetLineIndex(diagnostic.Span.Start);
+                    var line = treeText.Lines[lineIndex];
+                    int lineNumber = lineIndex + 1;
+                    int character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    TextSpan prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    TextSpan suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    string prefix = treeText.ToString(prefixSpan);
+                    string error = treeText.ToString(diagnostic.Span);
+                    string suffix = treeText.ToString(suffixSpan);
+
+                    Console.Write($"   {prefix}");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(error);
+                    Console.ResetColor();
+                    Console.Write(suffix);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
