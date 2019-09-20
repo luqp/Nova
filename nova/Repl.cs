b6a26e65bcff609edf205630f1b518e22d7c8536
Luqp <luceroqp@outlook.com>
2019-09-20 15:46:15 -0400

    MVP, Convert input data lines into a document and handle key modifiers - 'Enter' and 'ControlEnter' return all document

--- nova/Repl.cs
+++ nova/Repl.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Text;
 
 namespace Nova
 {
     internal abstract class Repl
     {
+        private string submissionText;
+
         public void Run()
         {
             while (true)
@@ -17,7 +21,209 @@ namespace Nova
             }
         }
 
+        private sealed class SubmissionView
+        {
+            private readonly ObservableCollection<string> submissionDocument;
+            private readonly int cursorTop;
+            private int renderedLineCount;
+            private int currentLineIndex;
+            private int currentCharacter;
+
+            public SubmissionView(ObservableCollection<string> submissionDocument)
+            {
+                this.submissionDocument = submissionDocument;
+                this.submissionDocument.CollectionChanged += SubmissionDocumentChanged;
+                cursorTop = Console.CursorTop;
+                Render();
+            }
+
+            private void SubmissionDocumentChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                Render();
+            }
+
+            private void Render()
+            {
+                Console.SetCursorPosition(0, cursorTop);
+                Console.CursorVisible = false;
+
+                int lineCount = 0;
+
+                foreach (string line in submissionDocument)
+                {
+                    if (lineCount == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("» ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("· ");
+                    }
+
+                    Console.ResetColor();
+                    Console.WriteLine(line);
+                    lineCount++;
+                }
+
+                int numberOfBlankLines = renderedLineCount - lineCount;
+
+                if (numberOfBlankLines > 0)
+                {
+                    string blankLine = new string(' ', Console.WindowWidth);
+                    while (numberOfBlankLines > 0)
+                    {
+                        Console.WriteLine(blankLine);
+                    }
+                }
+
+                renderedLineCount = lineCount;
+                Console.CursorVisible = true;
+
+                UpdateCursorPosition();
+
+            }
+
+            private void UpdateCursorPosition()
+            {
+                Console.CursorTop = cursorTop + currentLineIndex;
+                Console.CursorLeft = 2 + currentCharacter;
+            }
+
+            public int CurrentLineIndex
+            {
+                get => currentLineIndex;
+                set
+                {
+                    if (currentLineIndex != value)
+                    {
+                        currentLineIndex = value;
+                        UpdateCursorPosition();
+                    }
+                }
+            }
+            public int CurrentCharacter
+            {
+                get => currentCharacter;
+                set
+                {
+                    if (currentCharacter != value)
+                    {
+                        currentCharacter = value;
+                        UpdateCursorPosition();
+                    }
+                }
+            }
+        }
+
         private string EditSubmission()
+        {
+            submissionText = null;
+            ObservableCollection<string> document = new ObservableCollection<string>() { "" };
+            SubmissionView view = new SubmissionView(document);
+
+            while (submissionText == null)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                HandleKey(key, document, view);
+
+            }
+
+            Console.WriteLine();
+            return submissionText;
+        }
+
+        private void HandleKey(ConsoleKeyInfo key, ObservableCollection<string> document, SubmissionView view)
+        {
+            if (key.Modifiers == default(ConsoleModifiers))
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        HandleEnter(document, view);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        HandleLeftArrow(document, view);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        HandleRightArrow(document, view);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        HandleUpArrow(document, view);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        HandleDownArrow(document, view);
+                        break;
+                }
+            }
+            else if (key.Modifiers == ConsoleModifiers.Control)
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        HandleControlEnter(document, view);
+                        break;
+                }
+            }
+            if (key.KeyChar >= ' ')
+                HandleTyping(document, view, key.KeyChar.ToString());
+        }
+
+        private void HandleEnter(ObservableCollection<string> document, SubmissionView view)
+        {
+            string submission = string.Join(Environment.NewLine, document);
+            if (IsCompleteSubmission(submission))
+            {
+                this.submissionText = submission;
+                return;
+            }
+
+            document.Add(string.Empty);
+            view.CurrentCharacter = 0;
+            view.CurrentLineIndex = document.Count - 1;
+        }
+
+        private void HandleControlEnter(ObservableCollection<string> document, SubmissionView view)
+        {
+            submissionText = string.Join(Environment.NewLine, document);
+        }
+
+        private void HandleLeftArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            if (view.CurrentCharacter > 0)
+                view.CurrentCharacter--;
+        }
+
+        private void HandleRightArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            string line = document[view.CurrentLineIndex];
+            if (view.CurrentCharacter < line.Length - 1)
+                view.CurrentCharacter++;
+        }
+
+        private void HandleUpArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            if (view.CurrentLineIndex > 0)
+                view.CurrentLineIndex--;
+        }
+
+        private void HandleDownArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            if (view.CurrentLineIndex < document.Count - 1)
+                view.CurrentLineIndex++;
+        }
+
+        private void HandleTyping(ObservableCollection<string> document, SubmissionView view, string text)
+        {
+            int lineIndex = view.CurrentLineIndex;
+            int start = view.CurrentCharacter;
+
+            document[lineIndex] = document[lineIndex].Insert(start, text);
+            view.CurrentCharacter += text.Length; 
+        }
+
+        private string EditSubmissionOld()
         {
             StringBuilder textBuilder = new StringBuilder();
 
