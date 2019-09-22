1eb3c05ddbd1772794f2d688eda8d10d70a7cbec
Luqp <luceroqp@outlook.com>
2019-09-21 22:43:23 -0400

    Prettify the output

--- nova/NovaRepl.cs
+++ nova/NovaRepl.cs
@@ -21,10 +21,15 @@ namespace Nova
             {
                 bool isKeyword = token.Kind.ToString().EndsWith("Keyword");
                 bool isNumber = token.Kind == SyntaxKind.NumberToken;
+                bool isIdentifier = token.Kind == SyntaxKind.IdentifierToken;
 
                 if (isKeyword)
                     Console.ForegroundColor = ConsoleColor.Blue;
-                else if (!isNumber)
+                else if (isIdentifier)
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                else if (isNumber)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                else
                     Console.ForegroundColor = ConsoleColor.DarkGray;
 
                 Console.Write(token.Text);
