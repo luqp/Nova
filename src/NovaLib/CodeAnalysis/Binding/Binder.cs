bfb0a23489ff4f8e7e3433211a30b96a8dad5647
Luqp <luceroqp@outlook.com>
2019-10-02 11:09:56 -0400

    Minor fixes to binding of call arguments

--- src/NovaLib/CodeAnalysis/Binding/Binder.cs
+++ src/NovaLib/CodeAnalysis/Binding/Binder.cs
@@ -450,10 +450,26 @@ namespace Nova.CodeAnalysis.Binding
 
             if (syntax.Arguments.Count != function.Parameters.Length)
             {
-                diagnostics.ReportWrongArgumentCount(syntax.Span, function.Name, function.Parameters.Length, syntax.Arguments.Count);
+                TextSpan span;
+                if (syntax.Arguments.Count > function.Parameters.Length)
+                {
+                    SyntaxNode firstExceedingNode;
+                    if (function.Parameters.Length > 0)
+                        firstExceedingNode = syntax.Arguments.GetSeparator(function.Parameters.Length - 1);
+                    else
+                        firstExceedingNode = syntax.Arguments[0];
+                    var lastExceedingArgument = syntax.Arguments[syntax.Arguments.Count - 1];
+                    span = TextSpan.FromBounds(firstExceedingNode.Span.Start, lastExceedingArgument.Span.End);
+                }
+                else
+                {
+                    span = syntax.CloseParenthesisToken.Span;
+                }
+                diagnostics.ReportWrongArgumentCount(span, function.Name, function.Parameters.Length, syntax.Arguments.Count);
                 return new BoundErrorExpression();
             }
 
+            bool hasErrors = false;
             for (int i = 0; i < syntax.Arguments.Count; i++)
             {
                 BoundExpression argument = boundArguments[i];
@@ -461,10 +477,13 @@ namespace Nova.CodeAnalysis.Binding
 
                 if (argument.Type != parameter.Type)
                 {
-                    diagnostics.ReportWrongArgumentType(syntax.Arguments[i].Span, parameter.Name, parameter.Type, argument.Type);
-                    return new BoundErrorExpression();
+                    if (argument.Type != TypeSymbol.Error)
+                        diagnostics.ReportWrongArgumentType(syntax.Arguments[i].Span, parameter.Name, parameter.Type, argument.Type);
+                    hasErrors = true;
                 }
             }
+            if (hasErrors)
+                return new BoundErrorExpression();
 
             return new BoundCallExpression(function, boundArguments.ToImmutable());
         }
